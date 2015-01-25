a40daa1a74a70c08b9f50dd74bc58bd800c843d9
Jacek Dominiak <jacek@effortable.com>
2015-01-25 21:00:22 +0100

    base tests

--- test/serverSpec.js
+++ test/serverSpec.js
@@ -7,6 +7,11 @@
 
 var expect = require('chai').expect;
 
+var request = require('request');
+
+var server = require('../server'),
+    config = require('../config');
+
 describe('server based tests', function() {
 
     it('should be true', function (done) {
@@ -14,4 +19,55 @@ describe('server based tests', function() {
         done();
     });
 
+    it('should be able to reach the server', function (done) {
+
+        var data = {};
+
+        var options = {
+            uri: 'http://' + config.app.HOST + ':' + config.app.PORT,
+            method: 'GET',
+            json: data
+        };
+
+        request(options, function (err, res, body) {
+            if (err) throw err;
+            expect(res.statusCode).to.equal(404);
+            expect(body).to.equal('Not Found');
+            done();
+        });
+    });
+
+    it('should be able to make deploy request', function (done) {
+        var data = {};
+
+        var options = {
+            uri: 'http://' + config.app.HOST + ':' + config.app.PORT,
+            method: 'GET',
+            json: data
+        };
+
+        request(options, function (err, res, body) {
+            if (err) throw err;
+            expect(res.statusCode).to.equal(404);
+            expect(body).to.equal('Not Found');
+            done();
+        });
+    });
+
+    it('should receive 404 when request is other than POST /deploy', function (done) {
+        var data = {};
+
+        var options = {
+            uri: 'http://' + config.app.HOST + ':' + config.app.PORT,
+            method: 'GET',
+            json: data
+        };
+
+        request(options, function (err, res, body) {
+            if (err) throw err;
+            expect(res.statusCode).to.equal(404);
+            expect(body).to.equal('Not Found');
+            done();
+        });
+    });
 });
